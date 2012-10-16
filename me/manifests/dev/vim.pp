f2cc0b9ca211c30b2cdcbc2751c533ff5447ca43
Julien Muetton <themouette@gmail.com>
2012-10-16 14:47:57 +0200

    Includes pathogen.
    Pathogen is a submodule dependancy in me/files/vendor directory
    To add pathogen resources, vsrepo direcive module is used

--- me/manifests/dev/vim.pp
+++ me/manifests/dev/vim.pp
@@ -4,13 +4,65 @@ class me::dev::vim {
       /(Ubuntu|Debian)/ => 'vim-nox',
   }
 
-  package { 'vim-nox':
+  package { 'vim':
     ensure => present,
     name   => $me::dev::vim::packagename,
   }
 
   file { "/home/${me::username}/.vimrc":
-    source   => 'puppet:///modules/me/dotfiles/vimrc',
+    source  => 'puppet:///modules/me/dotfiles/vimrc',
     require => Class['me::user'],
+  }
+
+  file { "/home/${me::username}/.vim":
+    ensure  => directory,
+    owner   => $me::username,
+    group   => $me::username,
+    mode    => '0644',
+    require => [ Class['me::user'], Package['vim'] ],
+  }
+
+  # installing pathogen
+  file { "/home/${me::username}/.vim/bundle":
+    ensure  => directory,
+    owner   => $me::username,
+    group   => $me::username,
+    mode    => '0644',
+    require => [ Package['vim'], File["/home/${me::username}/.vim"] ],
+  }
+
+  file { "/home/${me::username}/.vim/autoload":
+    ensure  => directory,
+    owner   => $me::username,
+    group   => $me::username,
+    mode    => '0775',
+    require => [ Package['vim'], File["/home/${me::username}/.vim"] ],
+  }
+
+  file { 'pathogen':
+    ensure  => present,
+    source  => 'puppet:///modules/me/vendor/pathogen/autoload/pathogen.vim',
+    name    => "/home/${me::username}/.vim/autoload/pathogen.vim",
+    mode    => '0664',
+    require => [
+        File["/home/${me::username}/.vim/autoload"],
+        File["/home/${me::username}/.vim/bundle"],
+    ],
+  }
+
+  # include general purpose vim bundles
+  # solarized is the color scheme
+  vcsrepo { "/home/${me::username}/.vim/bundle/vim-colors-solarized":
+    ensure   => present,
+    provider => git,
+    source   => 'git://github.com/altercation/vim-colors-solarized.git',
+    require  => File['pathogen'],
+  }
+  # fugitive.vim is a git client
+  vcsrepo { "/home/${me::username}/.vim/bundle/vim-fugitive":
+    ensure   => present,
+    provider => git,
+    source   => 'git://github.com/tpope/vim-fugitive.git',
+    require  => File['pathogen'],
   }
 }
