27b4c5d0553cf94a3f30f2fe952355a519282e93
Julien Muetton <themouette@gmail.com>
2012-10-17 12:31:23 +0200

    me::user bash ressources are under me/files/bash

--- me/manifests/user.pp
+++ me/manifests/user.pp
@@ -9,42 +9,42 @@ class me::user {
   }
 
   file { "/home/${me::username}/.bash_logout":
-    source => 'puppet:///modules/me/dotfiles/bash_logout',
+    source => 'puppet:///modules/me/bash/bash_logout',
     owner  => $me::username,
     group  => $me::username,
     mode   => '0755',
   }
 
   file { "/home/${me::username}/.bashrc":
-    source => 'puppet:///modules/me/dotfiles/bashrc',
+    source => 'puppet:///modules/me/bash/bashrc',
     owner  => $me::username,
     group  => $me::username,
     mode   => '0755',
   }
 
   file { "/home/${me::username}/.bash_aliases":
-    source => 'puppet:///modules/me/dotfiles/bash_aliases',
+    source => 'puppet:///modules/me/bash/bash_aliases',
     owner  => $me::username,
     group  => $me::username,
     mode   => '0755',
   }
 
   file { "/home/${me::username}/.bash_completion":
-    source => 'puppet:///modules/me/dotfiles/bash_completion',
+    source => 'puppet:///modules/me/bash/bash_completion',
     owner  => $me::username,
     group  => $me::username,
     mode   => '0755',
   }
 
   file { "/home/${me::username}/.bash_functions":
-    source => 'puppet:///modules/me/dotfiles/bash_functions',
+    source => 'puppet:///modules/me/bash/bash_functions',
     owner  => $me::username,
     group  => $me::username,
     mode   => '0755',
   }
 
   file { "/home/${me::username}/.bash_prompt":
-    source => 'puppet:///modules/me/dotfiles/bash_prompt',
+    source => 'puppet:///modules/me/bash/bash_prompt',
     owner  => $me::username,
     group  => $me::username,
     mode   => '0755',
