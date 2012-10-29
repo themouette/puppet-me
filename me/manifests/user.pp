75771dc32739f004817495fb9c57bc2ca7328641
Julien Muetton <themouette@gmail.com>
2012-10-29 14:21:30 +0100

    Add ackconfiguration

--- me/manifests/user.pp
+++ me/manifests/user.pp
@@ -8,47 +8,43 @@ class me::user {
     password   => $me::password
   }
 
-  file { "/home/${me::username}/.bash_logout":
+  file {
+  "/home/${me::username}/.bash_logout":
     source => 'puppet:///modules/me/bash/bash_logout',
     owner  => $me::username,
     group  => $me::username,
-    mode   => '0755',
-  }
-
-  file { "/home/${me::username}/.bashrc":
+    mode   => '0755';
+  "/home/${me::username}/.bashrc":
     source => 'puppet:///modules/me/bash/bashrc',
     owner  => $me::username,
     group  => $me::username,
-    mode   => '0755',
-  }
-
-  file { "/home/${me::username}/.bash_aliases":
+    mode   => '0755';
+  "/home/${me::username}/.bash_aliases":
     source => 'puppet:///modules/me/bash/bash_aliases',
     owner  => $me::username,
     group  => $me::username,
-    mode   => '0755',
-  }
-
-  file { "/home/${me::username}/.bash_completion":
+    mode   => '0755';
+  "/home/${me::username}/.bash_completion":
     source => 'puppet:///modules/me/bash/bash_completion',
     owner  => $me::username,
     group  => $me::username,
-    mode   => '0755',
-  }
-
-  file { "/home/${me::username}/.bash_functions":
+    mode   => '0755';
+  "/home/${me::username}/.bash_functions":
     source => 'puppet:///modules/me/bash/bash_functions',
     owner  => $me::username,
     group  => $me::username,
-    mode   => '0755',
-  }
-
-  file { "/home/${me::username}/.bash_prompt":
+    mode   => '0755';
+  "/home/${me::username}/.bash_prompt":
     source => 'puppet:///modules/me/bash/bash_prompt',
     owner  => $me::username,
     group  => $me::username,
-    mode   => '0755',
+    mode   => '0755';
+  "/home/${me::username}/.ackrc":
+    source => 'puppet:///modules/me/ackrc',
+    owner  => $me::username,
+    group  => $me::username,
+    mode   => '0755';
   }
 
-  include me::user::sudoer
+  include me::user::sudoer, me::user::ssh
 }
