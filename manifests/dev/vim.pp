ea9ab9c994ad02bc4cc3d5f1e07352f5daaded87
Julien Muetton <themouette@gmail.com>
2013-12-01 00:39:01 +0100

    Update me::dev::vim with params and helpers.

--- manifests/dev/vim.pp
+++ manifests/dev/vim.pp
@@ -16,15 +16,15 @@ class me::dev::vim {
     ensure  => present,
   }
 
-  file { "/home/${me::username}/.vimrc":
+  file { "${::me::params::home}/.vimrc":
     source  => 'puppet:///modules/me/vim/vimrc',
     require => Class['me::user'],
   }
 
-  file { "/home/${me::username}/.vim":
+  file { "${::me::params::home}/.vim":
     ensure  => directory,
-    owner   => $me::username,
-    group   => $me::username,
+    owner   => $::me::params::username,
+    group   => $::me::params::username,
     mode    => '0644',
     require => [ Class['me::user'], Package['vim'] ],
   }
@@ -33,135 +33,84 @@ class me::dev::vim {
   # ===================
   # directory structure
   file {
-  ["/home/${me::username}/.vim/bundle",
-  "/home/${me::username}/.vim/autoload",
-  "/home/${me::username}/.vim/ftplugin",
-  "/home/${me::username}/.vim/plugin",
-  "/home/${me::username}/.vim/syntax",
-  "/home/${me::username}/.vim/ftdetect",
-  "/home/${me::username}/.vim/snippets",
-  "/home/${me::username}/.vim/UltiSnips",
+  ["${::me::params::home}/.vim/bundle",
+  "${::me::params::home}/.vim/autoload",
+  "${::me::params::home}/.vim/ftplugin",
+  "${::me::params::home}/.vim/plugin",
+  "${::me::params::home}/.vim/syntax",
+  "${::me::params::home}/.vim/ftdetect",
+  "${::me::params::home}/.vim/snippets",
+  "${::me::params::home}/.vim/UltiSnips",
     ]:
     ensure  => directory,
-    owner   => $me::username,
-    group   => $me::username,
+    owner   => $::me::params::username,
+    group   => $::me::params::username,
     mode    => '0775',
-    require => [ Package['vim'], File["/home/${me::username}/.vim"] ];
+    require => [ Package['vim'], File["${::me::params::home}/.vim"] ];
   }
   # pathogen plugin file
   file {
   'pathogen':
     ensure  => present,
     source  => 'puppet:///modules/me/vendor/pathogen/autoload/pathogen.vim',
-    name    => "/home/${me::username}/.vim/autoload/pathogen.vim",
-    owner   => $me::username,
-    group   => $me::username,
+    name    => "${::me::params::home}/.vim/autoload/pathogen.vim",
+    owner   => $::me::params::username,
+    group   => $::me::params::username,
     mode    => '0664',
     require => [
-        File["/home/${me::username}/.vim/autoload"],
-        File["/home/${me::username}/.vim/bundle"],
-        File["/home/${me::username}/.vim/ftplugin"],
-        File["/home/${me::username}/.vim/plugin"],
-        File["/home/${me::username}/.vim/syntax"],
-        File["/home/${me::username}/.vim/ftdetect"],
-        File["/home/${me::username}/.vim/snippets"],
-        File["/home/${me::username}/.vim/UltiSnips"],
+        File["${::me::params::home}/.vim/autoload"],
+        File["${::me::params::home}/.vim/bundle"],
+        File["${::me::params::home}/.vim/ftplugin"],
+        File["${::me::params::home}/.vim/plugin"],
+        File["${::me::params::home}/.vim/syntax"],
+        File["${::me::params::home}/.vim/ftdetect"],
+        File["${::me::params::home}/.vim/snippets"],
+        File["${::me::params::home}/.vim/UltiSnips"],
     ];
-  "/home/${me::username}/.vim/plugin/me.vim":
-    ensure  => present,
-    content => template('me/vim/plugin/me.vim.erb'),
-    owner   => $me::username,
-    group   => $me::username,
-    mode    => '0775',
-    require => [ File['pathogen'] ];
-  "/home/${me::username}/.vim/autoload/snippets.vim":
-    ensure  => present,
-    source  => 'puppet:///modules/me/vim/autoload/snippets.vim',
-    owner   => $me::username,
-    group   => $me::username,
-    mode    => '0775',
-    require => [ File['pathogen'] ];
-  "/home/${me::username}/.vim/ftplugin/markdown.vim":
-    ensure  => present,
-    source  => 'puppet:///modules/me/vim/ftplugin/markdown.vim',
-    owner   => $me::username,
-    group   => $me::username,
-    mode    => '0775',
-    require => [ File['pathogen'] ];
-  "/home/${me::username}/.vim/python":
-    ensure  => present,
-    source  => 'puppet:///modules/me/vim/python',
-    owner   => $me::username,
-    group   => $me::username,
-    recurse => true,
-    mode    => '0775',
-    require => [ File['pathogen'] ];
+  }
+
+  # Copy vim config files
+  me::dev::vim::file {
+    'plugin/me.vim': ;
+    'ftplugin/markdown.vim': ;
+    # python helpers for snippets
+    'autoload/snippets.vim': ; 'python': ;
   }
 
   # general purpose vim bundles
   # ===========================
-  # solarized is the color scheme
-  vcsrepo { "/home/${me::username}/.vim/bundle/vim-colors-solarized":
-    ensure   => present,
-    provider => git,
-    user     => $me::username,
-    source   => 'git://github.com/altercation/vim-colors-solarized.git',
-    require  => File['pathogen'],
-  }
-  # fugitive.vim is a git client
-  vcsrepo { "/home/${me::username}/.vim/bundle/vim-fugitive":
-    ensure   => present,
-    provider => git,
-    user     => $me::username,
-    source   => 'git://github.com/tpope/vim-fugitive.git',
-    require  => File['pathogen'],
-  }
-  # UltiSnips
-  vcsrepo { "/home/${me::username}/.vim/bundle/ultisnips":
-    ensure   => present,
-    provider => git,
-    user     => $me::username,
-    source   => 'git://github.com/SirVer/ultisnips.git',
-    require  => File['pathogen'],
-  }
-  # TagBar
-  vcsrepo { "/home/${me::username}/.vim/bundle/tagbar":
-    ensure   => present,
-    provider => git,
-    user     => $me::username,
-    source   => 'git://github.com/majutsushi/tagbar.git',
-    require  => File['pathogen'],
-  }
-  # vim ack
-  vcsrepo { "/home/${me::username}/.vim/bundle/vim-ack":
-    ensure   => present,
-    provider => git,
-    user     => $me::username,
-    source   => 'git://github.com/mileszs/ack.vim.git',
-    require  => File['pathogen'],
-  }
-  # vim powerline
-  vcsrepo { "/home/${me::username}/.vim/bundle/vim-powerline":
-    ensure   => present,
-    provider => git,
-    user     => $me::username,
-    source   => 'git://github.com/Lokaltog/vim-powerline.git',
-    require  => File['pathogen'],
-  }
-  # syntastic
-  vcsrepo { "/home/${me::username}/.vim/bundle/syntastic":
-    ensure   => present,
-    provider => git,
-    user     => $me::username,
-    source   => 'https://github.com/scrooloose/syntastic.git',
-    require  => File['pathogen'],
-  }
-  # vim markdown
-  vcsrepo { "/home/${me::username}/.vim/bundle/vim-markdown":
-    ensure   => present,
-    provider => git,
-    user     => $me::username,
-    source   => 'git://github.com/tpope/vim-markdown.git',
-    require  => File['pathogen'],
+  me::dev::vim::bundle {
+    # solarized is the color scheme
+    'vim-colors-solarized':
+      source => 'git://github.com/altercation/vim-colors-solarized.git',
+      ;
+    # fugitive.vim is a git client
+    'vim-fugitive':
+      source => 'git://github.com/tpope/vim-fugitive.git',
+      ;
+    # UltiSnips
+    'ultisnips':
+      source => 'git://github.com/SirVer/ultisnips.git',
+      ;
+    # TagBar
+    'tagbar':
+      source => 'git://github.com/majutsushi/tagbar.git',
+      ;
+    # vim ack
+    'vim-ack':
+      source => 'git://github.com/mileszs/ack.vim.git',
+      ;
+    # vim powerline replacement
+    'vim-airline':
+      source => 'https://github.com/bling/vim-airline.git',
+      ;
+    # syntastic
+    'syntastic':
+      source => 'https://github.com/scrooloose/syntastic.git',
+      ;
+    # vim markdown
+    'vim-markdown':
+      source => 'git://github.com/tpope/vim-markdown.git',
+      ;
   }
 }
